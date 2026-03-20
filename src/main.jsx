958eb7372fb6cacf14051089ef26b500dc7414e0
Manny605 <abderahmanecams5@gmail.com>
2026-03-20 16:11:11 +0000

    update200326

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './styles/global.css';
-import { getStoredTheme, applyTheme } from './store/theme.js';
-applyTheme(getStoredTheme());
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {});
-  });
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode><App /></StrictMode>
